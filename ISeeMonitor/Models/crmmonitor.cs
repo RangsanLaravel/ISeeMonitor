d1a7602d484e21e8176a2e44150c204ae296176f
rangsanchipphang <rangsanchipphang@RANGSANCHIPB992>
2023-11-02 01:21:37 +0700

    add column

--- ISeeMonitor/Models/crmmonitor.cs
+++ ISeeMonitor/Models/crmmonitor.cs
@@ -10,11 +10,22 @@ namespace ISeeMonitor.Models
     {
         public string customer_id { get; set; }
         public string fname { get; set; }
+        public string website { get; set; }
+        public string contract { get; set; }
+        public string position { get; set; }
         public string email { get; set; }
         public string phone_no { get; set; }
         public string owner { get; set; }
-        public string tranfer_to { get; set; }
+        public string Model_type { get; set; }
         public string location_name { get; set; }
+        public string People { get; set; }
+        public string Priority { get; set; }
+        public string Deal_creation_date { get; set; }
+        public string Due_date_follow_up { get; set; }
+        public string Deal_value { get; set; }
+        public string Percen { get; set; }
+        public string Won { get; set; }
+        public string tranfer_to { get; set; }
         public string job_id { get; set; }
         public string jobdescription { get; set; }
         public string job_status_code { get; set; }
